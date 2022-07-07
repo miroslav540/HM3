423e4eaf8ded33d81880cf51c5af4fceb00bc788
Miroslav <mir19972905@mai.ru>
2022-07-07 22:57:54 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿void mathpoww(double A, double B)
+{
+    double Sum = 0;
+    for (double i = 0; i < A; i++)
+    {
+        Sum = Math.Pow(A, B);
+    }
+    Console.WriteLine(Sum);
+}
+mathpoww(3,5);
+mathpoww(2,4);
