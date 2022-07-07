aab686f78e95e9eb962864f7d9d3c0f8c46850a5
Miroslav <mir19972905@mai.ru>
2022-07-07 22:58:06 +0300

    task 27

---
+++ task27/Program.cs
@@ -0,0 +1,23 @@
+﻿static void summaArray(string[] args)
+{
+    int n;
+    Console.WriteLine("Введите размер массива");
+    n = int.Parse(Console.ReadLine());
+    int[] massiv = new int[n];
+    Console.WriteLine("Введите элементы массива: "); //тут вводить по очереди
+    for (int i = 0; i < n; i++)
+    {
+        massiv[i] = int.Parse(Console.ReadLine());
+    }
+    int Sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+       Sum+=massiv[i];
+    }
+    Console.WriteLine("Сумма="+Sum);
+}
+summaArray(args);
+/* по заданию 452 -> 11
+82 -> 10
+9012 -> 12, но можно любые
+*/
