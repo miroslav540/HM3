00b3df02fd12c3f67b589981a59b9e07052d00d9
Miroslav <mir19972905@mai.ru>
2022-07-07 22:58:14 +0300

    task 29

---
+++ task29/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array ()    // сделал усложненую версию
+{
+    Console.WriteLine("Введите количество цифр");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int[] result = new int [num];
+    for (int i = 0; i < num; i++) // если сделать <= то получается ошибка, надо посмотреть почему
+    {
+        int A = (new Random()).Next(0,100); // решил от 0 до 100
+        result[i] = A;
+        Console.Write(result[i] + " ");    
+    }
+
+    return result;
+}
+
+array();
+
+//не обращайте внимание на заметки, это я для себя ^_^
